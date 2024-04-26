a6ca2b7d5ba0350fe09b0103e75fd7378e495c7e
vrossum <mark.vanrossum@nottingham.ac.uk>
2024-04-26 21:51:31 +0100

    Delete hazardfunE.m

--- hazardfunE.m
+++
@@ -1,5 +0,0 @@
-% assumed relation between hazard and reserve.
-function h = hazardfunE(E,p)
-    h= exp(-p.ch*max(E,0));
-    % note changing this necessitates recalibration of EspentLTM (Emax, cLTM)
-end
